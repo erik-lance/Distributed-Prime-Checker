cde4340224bcd8ed85a9d1ed72a240fe593c83dd
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-23 03:20:13 +0800

    Added initial master class

---
+++ Master.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * This is the master server. It is responsible for waiting for a request from a client, which is the range
+ * of numbers to check prime numbers, and then sends a message to slave servers to check the prime numbers.
+ */
+class Master
+{
+};
+
