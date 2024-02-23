6e77c72a7eada8d09dd62ce363f202661f12528c
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-23 11:29:18 +0800

    Added initial master server

--- Master.cpp
+++ Master.cpp
@@ -1,1 +1,53 @@
 #include "Master.h"
+
+Master::Master(std::string host_address, int port_number)
+{
+	this->host = host_address;
+	this->port = port_number;
+
+	init();
+}
+
+Master::~Master()
+{
+}
+
+void Master::init()
+{
+	// Create a socket
+	m_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (m_socket < 0)
+	{
+		std::cerr << "Error creating socket" << std::endl;
+		exit(1);
+	}
+
+	// Set up the server address
+	memset((char*)&m_server, 0, sizeof(m_server));
+	m_server.sin_family = AF_INET;
+	m_server.sin_port = htons(port);
+	m_server.sin_addr.s_addr = inet_addr(host.c_str());
+
+	// Bind the socket to the server address
+	if (bind(m_socket, (struct sockaddr*)&m_server, sizeof(m_server)) < 0)
+	{
+		std::cerr << "Error binding socket" << std::endl;
+		exit(1);
+	}
+}
+
+void Master::loop()
+{
+}
+
+void Master::start()
+{
+}
+
+void Master::send(std::string message)
+{
+}
+
+void Master::receive()
+{
+}
--- Master.h
+++ Master.h
@@ -1,10 +1,53 @@
 #pragma once
 
+#include <iostream>
+#include <thread>
+#include <string>
+#include <queue>
+#include "PrimeChecker.h"
+
+#ifdef _WIN32
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#endif
+
 /**
  * This is the master server. It is responsible for waiting for a request from a client, which is the range
  * of numbers to check prime numbers, and then sends a message to slave servers to check the prime numbers.
  */
 class Master
 {
+public:
+	Master(std::string host_address, int port_number);
+	~Master();
+
+private:
+	std::string host; // Host Address
+	int port; // Port Number
+
+	// Socket
+	int m_socket;
+	struct sockaddr_in m_server; // Server Address
+
+	// Queue for messages
+	std::queue<std::string> queue;
+
+	// Threads
+	std::thread listener;
+	std::thread sender;
+
+	// Functions
+	void init();
+	void loop();
+	void start();
+	void send(std::string message);
+	void receive();
 };
 
