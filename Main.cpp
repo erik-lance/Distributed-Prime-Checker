2a25329949c2db628e713c9e5645e8653195da91
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-23 11:24:47 +0800

    Added initialization of machine process type

--- Main.cpp
+++ Main.cpp
@@ -2,5 +2,33 @@
 
 int main()
 {
+	// Check process type using env
+	if (getenv("PROCESS_TYPE") == NULL)
+	{
+		std::cout << "Please set the PROCESS_TYPE environment variable" << std::endl;
+		return 1;
+	}
+
+	std::string process_type = getenv("PROCESS_TYPE");
+
+	// Prepare as master server or slave server
+	if (process_type == "MASTER")
+	{
+		// Master server
+		std::string host = "127.0.0.1";
+		int port = getenv("PORT") == NULL ? 6379 : atoi(getenv("PORT"));
+	}
+	else if (process_type == "CLIENT")
+	{
+		// Client
+		std::string host = "127.0.0.1";
+		int port = getenv("PORT") == NULL ? 6379 : atoi(getenv("PORT"));
+	}
+	else
+	{
+		// Slave server
+
+	}
+
 	return 0;
 }
--- Main.h
+++ Main.h
@@ -1,3 +1,7 @@
 #pragma once
 #include <iostream>
+#include <string>
 
+#include "Client.h"
+#include "Server.h"
+#include "Master.h"
