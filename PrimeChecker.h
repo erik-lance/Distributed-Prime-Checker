ae966673979a8b30951648d4c0b44c6fdaecfcd2
Michael-Mangoba <99485815+Michael-Mangoba@users.noreply.github.com>
2024-02-24 12:50:36 +0800

    Added Prime Checker function

--- PrimeChecker.h
+++ PrimeChecker.h
@@ -1,5 +1,24 @@
 #pragma once
 
+/**
+ * This function checks if a number is prime.
+ * @param n The number to check.
+ * @return true if the number is prime, false otherwise.
+ */
 static inline bool isPrime(int n) {
-	return 0;
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+
+	for (int i = 5; i * i <= n; i += 6)
+	{
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+	}
+
+	return true;
 }
