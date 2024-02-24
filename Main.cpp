83673bfaade31a684dff5616e7517f92eca74cb5
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-24 20:17:38 +0800

    Simplified process type

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,11 @@ int main()
 {
 	// Ask process type
 	std::string process_type;
-	std::cout << "Enter process type (MASTER, SLAVE, CLIENT): ";
+	std::cout << "Enter process type (0 - MASTER, 1 - SLAVE, 2 - CLIENT): ";
 	std::cin >> process_type;
 
 	// Prepare as master server or slave server
-	if (process_type == "MASTER")
+	if (process_type == "0")
 	{
 		// Master server
 		std::string full_address = master_address;
@@ -19,7 +19,7 @@ int main()
 		Master master(host, port);
 		master.start();
 	}
-	else if (process_type == "CLIENT")
+	else if (process_type == "2")
 	{
 		// Client
 		std::string full_address = client_address;
